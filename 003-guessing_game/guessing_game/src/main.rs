195466e4b2b10cc23283f2913545d05230c288f9
recelilious <kylewu2004@outlook.com>
2025-12-21 18:47:07 +1000

    guessing_game-2025.12.21

--- 003-guessing_game/guessing_game/src/main.rs
+++ 003-guessing_game/guessing_game/src/main.rs
@@ -1,13 +1,38 @@
+use std::cmp::Ordering;
 use std::io;
 
+use rand::Rng;
+
 fn main() {
-    println!("Guess the number:");
+    let sn = rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is: {}", sn);
 
-    let mut guess = String::new();
+    loop {
+        println!("Guess the number:");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        let mut guess = String::new();
 
-    println!("You guessed {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&sn) {
+            Ordering::Less => println!("Small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Greater => println!("Big!"),
+        }
+    }
 }
